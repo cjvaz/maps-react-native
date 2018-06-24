c987a4b0e55d33173b73542abbfcfa7dcccf34ea
Carlos Vaz <carlosjvaz@gmail.com>
2018-06-24 14:43:00 -0300

    Places list

--- App.js
+++ App.js
@@ -1,44 +1,132 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, Dimensions, StyleSheet, ScrollView, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
+import Reactotron from 'reactotron-react-native'
+
+const { height, width } = Dimensions.get('window');
+
+
+export default class App extends Component {
+
+  state= {
+    latitude: -27.2106710,
+    longitude: -49.6362700,
+    places: [
+      {
+        id: 1,
+        title: 'Mercado Publico',
+        description: 'Barracas de frutas, peixes...',
+        latitude: -30.0275784,
+        longitude: -51.2281022,
+      },
+      {
+        id: 2,
+        title: 'Aeroporto Salgado Filho',
+        description: 'Voos internacionais e nacionais',
+        latitude: -29.9934732,
+        longitude: -51.1775698,
+      },
+      {
+        id: 3,
+        title: 'Parque da Redenção',
+        description: 'Lazer, passeios',
+        latitude: -30.0375334,
+        longitude: -51.2176093,
+      }
+    ]    
+  }
+
+  _mapReady = () => {
+    this.state.places[0].mark.showCallout();
+  }
+  
+  render() {
+    
+    const { latitude, longitude } = this.state.places[0];
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref={map => this.mapView = map}
+          region={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0142,
+            longitudeDelta: 0.0231,
+          }}
+          style={styles.map}
+          rotateEnabled={false}
+          scrollEnabled={false}
+          zoomEnabled={false}
+          showsPointsOfInterest={false}
+          showsBuildings={false}
+          onMapReady={this._mapReady}
+        >
+          { this.state.places.map(place => (
+            <MapView.Marker
+            ref={mark => place.mark = mark}
+            title={place.title}
+            description={place.description}
+            key={place.id}
+            coordinate={{
+              latitude: place.latitude,
+              longitude: place.longitude
+            }} />
+          ))}
+
+        </MapView>
+        <ScrollView
+          style={styles.placesContainer}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          onMomentumScrollEnd={(e) => {
+            const place = ( e.nativeEvent.contentOffset.x > 0)
+              ? e.nativeEvent.contentOffset.x / width 
+              : 0;
+            placeRound =  Math.round(place);
+            const { latitude, longitude, mark } = this.state.places[placeRound];
+            this.mapView.animateToCoordinate({ latitude, longitude }, 1000);
+            setTimeout(() => {
+              mark.showCallout();
+            }, 1000)
+          }}
+        >
+          { this.state.places.map(place => (
+            <View key={place.id} style={styles.place}>
+              <Text>{place.title}</Text>
+              <Text>{place.description}</Text>
+            </View>
+          ))} 
+          
+        </ScrollView>
+      </View>      
+
+    );
+  }
+  
+}
 
 const styles = StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center',
+    alignItems: 'flex-end',
   },
   map: {
     ...StyleSheet.absoluteFillObject,
   },
-});
 
-export default class App extends Component {
-  render() {
-    const { region } = this.props;
-    console.log(region);
+  placesContainer: {
+    width: '100%',
+    maxHeight: 200
+  },
 
-    return (
-      <View style ={styles.container}>
-        <MapView
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}
-        >
-        </MapView>
-      </View>
-    );
+  place: {
+    width: width - 40,
+    maxHeight: 200,
+    backgroundColor: '#FFF',
+    marginHorizontal: 20
   }
-}
 
+});
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import './ReactotronConfig';
 import { AppRegistry } from 'react-native';
 import App from './App';
+
+
 
 AppRegistry.registerComponent('testMaps', () => App);
